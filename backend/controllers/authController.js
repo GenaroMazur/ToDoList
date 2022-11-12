25560fecdf5e880b01874bbcfb9c6f1014fa7c3d
genaro mazur <genaromazur03@gmail.com>
2022-11-12 00:10:00 -0300

    [ADD & MOD] [ADD] *helpers folder [MOD] *authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,34 +1,52 @@
 const { User } = require("./../database/models")
 const bcrypt = require("bcryptjs")
+const { endpointResponse } = require("../helpers/success");
+const { catchAsync } = require("../helpers/catchAsync");
+const createHttpError = require("http-errors");
 
 const authController = {
-    login: async (req, res)=>{
-        res.json({
-            status:200,
-            token:req.token
-        })
-    },
+    login: catchAsync(async (req, res, next) => {
+        try{
 
-    register: (req, res)=>{
-        const hashedPass = bcrypt.hashSync(req.body.password)
-        const user={
-            userName:req.body.username,
-            email:req.body.email,
-            userPassword:hashedPass,
-            roleId:2
-        }
-        User.create(user)
-        .then(user=>{
-            user.password=undefined
-            res.json({
+            endpointResponse({
+                res,
                 status:200,
-                body:user
+                message:"login user sucefull",
+                token:req.token
             })
-        })
-        .catch(err=>{
-            res.json({err})
-        })
-    }
+        } catch(err){
+            const httpError = createHttpError(
+                err.statusCode,
+                `[Error retrieving index] - [index - GET]: ${err.message}`
+            );
+            next(httpError);
+        }
+    }),
+    register:catchAsync(async (req, res, next) => {
+        try{
+
+            const hashedPass = bcrypt.hashSync(req.body.password)
+            const user={
+                userName:req.body.username,
+                email:req.body.email,
+                userPassword:hashedPass,
+                roleId:2
+            }
+        const response = User.create(user)
+            endpointResponse({
+                res,
+                status:200,
+                message:"login register sucefull",
+                token:response
+            })
+        } catch(err){
+            const httpError = createHttpError(
+                err.statusCode,
+                `[Error retrieving index] - [index - GET]: ${err.message}`
+            );
+            next(httpError);
+        }
+    })
 }
 
 module.exports= authController
