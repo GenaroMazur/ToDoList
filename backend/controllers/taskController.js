dc95ce3f91ec5709d28ec79967b74a9de1aa9fd8
genaro mazur <genaromazur03@gmail.com>
2022-11-12 22:32:33 -0300

    [MOD feat] *taskController (delete and update function)

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -5,47 +5,48 @@ const createHttpError = require("http-errors");
 const link = "http://localhost:3000/"
 
 const taskController = {
-    taskList:catchAsync(async (req, res, next) => {
-        try{
+    taskList: catchAsync(async (req, res, next) => {
+        try {
             const user = req.user
             const userId = req.query.userId || user.id
-            let condition={userId:userId}
+            let condition = { userId: userId }
 
-            if(user.role=="admin" && req.query.userId==undefined){
-                condition=1
-            } 
-            
+            if (user.role == "admin" && req.query.userId == undefined) {
+                condition = 1
+            }
+
             const response = await Task.findAll({
-                where:condition,
-                include:[
+                where: condition,
+                include: [
                     {
-                        association:"TaskStatus",
-                        attributes:["id","status","description"]
+                        association: "TaskStatus",
+                        attributes: ["id", "status", "description"]
                     }
                 ],
-                attributes:["id","tittle","description","expirationDate",[sequelize.fn("CONCAT",link,"task/",sequelize.col("task.id")),"detail"],"userId"]
+                attributes: ["id", "tittle", "description", "expirationDate", [sequelize.fn("CONCAT", link, "task/", sequelize.col("task.id")), "detail"], "userId"]
             })
 
-            const count = await Task.count({where:condition})
+            const count = await Task.count({ where: condition })
 
-            if( req.query.userId && response[0] ){
-                if((response[0].dataValues.userId != user.id) && user.role != "admin"){
-                return endpointResponse({
-                    res,
-                    code:401,
-                    message:"you don't have permissions",
-                })
-            }}
+            if (req.query.userId && response[0]) {
+                if ((response[0].dataValues.userId != user.id) && user.role != "admin") {
+                    return endpointResponse({
+                        res,
+                        code: 401,
+                        message: "you don't have permissions",
+                    })
+                }
+            }
 
             return endpointResponse({
                 res,
-                message:"tasks received sucefull",
-                body:{
+                message: "tasks received sucefull",
+                body: {
                     count,
-                    tasks:response
+                    tasks: response
                 }
             })
-        } catch(err){
+        } catch (err) {
             console.log(err);
             const httpError = createHttpError(
                 err.statusCode,
@@ -55,56 +56,157 @@ const taskController = {
         }
     }),
 
-    taskDetail:catchAsync(async (req, res, next)=>{
-            try {
-                const user = req.user
-                const taskId = 1
-                const linkUser = [sequelize.fn("CONCAT",link,"user/",sequelize.col("user.id")),"detail"]
+    taskDetail: catchAsync(async (req, res, next) => {
+        try {
+            const user = req.user
+            const taskId = req.params.id
+            const linkUser = [sequelize.fn("CONCAT", link, "user/", sequelize.col("user.id")), "detail"]
 
-                const response = await Task.findOne({
-                    where:{id:taskId},
-                    attributes:{exclude:["taskStatusId", "userId","UserId"]},
-                    include:[
-                        {"association":"User", attributes:["id","userName","email",linkUser]},
-                        {"association":"TaskStatus",attributes:{exclude:["createdAt","updatedAt"]}}
-                    ]
+            const response = await Task.findOne({
+                where: { id: taskId },
+                attributes: { exclude: ["taskStatusId", "userId", "UserId"] },
+                include: [
+                    { "association": "User", attributes: ["id", "userName", "email", linkUser] },
+                    { "association": "TaskStatus", attributes: { exclude: ["createdAt", "updatedAt"] } }
+                ]
+            })
+            if (response == null) {
+                return endpointResponse({
+                    res,
+                    message: "task dont exist",
+                    body: null
+                })
+            }
+            if (response.User.id == user.id || user.role == "admin") {
+                endpointResponse({
+                    res,
+                    message: "task received sucefull",
+                    body: response
+                })
+            } else {
+                endpointResponse({
+                    res,
+                    code: 401,
+                    message: "you don't have permissions"
                 })
 
-                if(response.User.id==user.id || user.role == "admin"){
-                    endpointResponse({
-                        res,
-                        message:"task received sucefull",
-                        body:response
-                    })
-                }else {
-                    endpointResponse({
-                        res,
-                        code:401,
-                        message:"you don't have permissions"
-                    })
-                    
-                }
-            } catch (error) {
-                const httpError = createHttpError(
-                    error.statusCode,
-                    `[Error retrieving index] - [index - GET]: ${error.message}`
-                );
-                next(httpError);
             }
-        })
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error retrieving index] - [index - GET]: ${error.message}`
+            );
+            next(httpError);
+        }
+    })
     ,
 
-    taskCreate:(req, res)=>{
+    taskCreate: catchAsync(async (req, res, next) => {
+        try {
+            if (!req.body.tittle) {
+                return endpointResponse({
+                    res,
+                    message: "the tittle field not be empty",
+                    code: 400
+                })
+            }
 
-    },
+            const user = req.user
+            const task = {
+                tittle: req.body.tittle,
+                description: req.body.description,
+                taskStatusId: 1,
+                expirationDate: req.body.expirationDate,
+                userId: user.id
+            }
+            const response = await Task.create(task)
+            endpointResponse({
+                res,
+                message: "task created sucefull",
+                body: response
+            })
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error retrieving index] - [index - GET]: ${error.message}`
+            );
+            next(httpError);
+        }
+    }),
 
-    taskDelete:(req, res)=>{
+    taskDelete: catchAsync(async (req, res, next) => {
+        try {
 
-    },
+            const user = req.user
+            const taskId = req.params.id
+            const response = await Task.findOne({ where: { id: taskId } })
+            if (response.dataValues.userId != user.id && user.role != "admin") {
+                return endpointResponse({
+                    res,
+                    message: "you don't have permissions",
+                    code: 401
+                })
+            }
+            const taskDeleted = await Task.destroy({
+                where: {
+                    id: taskId
+                }
+            })
+            endpointResponse({
+                res,
+                message: "task created sucefull",
+                body: taskDeleted
+            })
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error retrieving index] - [index - GET]: ${error.message}`
+            );
+            next(httpError);
+        }
+    }),
 
-    taskUpdate:(req, res)=>{
+    taskUpdate: catchAsync(async (req, res, next) => {
+        try {
+            if (!req.body.tittle) {
+                return endpointResponse({
+                    res,
+                    message: "the tittle field not be empty",
+                    code: 400
+                })
+            }
+            const task = {
+                tittle: req.body.tittle,
+                description: req.body.description,
+                taskStatusId: req.body.taskStatusId,
+                expirationDate: req.body.expirationDate
+            }
+            const taskId = req.params.id
+            const user = req.user
+            const response = await Task.findOne({ where: { id: taskId } })
+            if (response.dataValues.userId != user.id && user.role != "admin") {
+                return endpointResponse({
+                    res,
+                    message: "you don't have permissions",
+                    code: 401
+                })
+            } else {
 
-    }
+                const updatedTask = await Task.update(task, { where: { id: taskId } })
+                endpointResponse({
+                    res,
+                    message: "task updated sucefull",
+                    body: updatedTask
+                })
+            }
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error retrieving index] - [index - GET]: ${error.message}`
+            );
+            next(httpError);
+        }
+    })
 }
 
-module.exports=taskController
+module.exports = taskController
