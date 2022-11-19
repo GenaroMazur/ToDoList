927107616106851e5e0bab96e05753de62a795f7
genaro mazur <genaromazur03@gmail.com>
2022-11-19 01:38:45 -0300

    [MOD] *Header .css .jsx

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,14 +1,14 @@
+import {Link} from "react-router-dom"
 import "./Header.css"
 const logo = require("./../../designs/logo.png")
-
 function Header(){
     return (
         <div className="header">
-            <h1 id="tittle">Task <span className="doneHeader">DONE</span></h1>
+            <Link to="/"><h1 id="tittle">Task <span className="doneHeader">DONE</span></h1></Link>
             <img src={logo} alt="task done logo" className="logoHeader"/>
             <div className="buttonsHeader">
-                <button id="buttonLogin">login</button>
-                <button id="buttonSignIn">sign in</button>
+                <Link to="/login"id="buttonLogin">login</Link>
+                <Link to="/register" id="buttonSignIn">sign in</Link>
             </div>
         </div>
     )
